03579aabad5303d18997669b97f317df2ee229ee
Clayton <clayton.almeida.campos@gmail.com>
2022-04-11 14:52:11 -0300

    Finalizacao automacao

---
+++ Makefile
@@ -0,0 +1,15 @@
+install:
+	@poetry install
+format:
+	@isort .
+	@blue .
+lint:
+	@blue . --check
+	@isort . --check
+	@prospector --with-tool pep257 --doc-warning
+test:
+	@pytest -v
+sec:
+	@pip-audit
+doc:
+	@mkdocs new .
--- tests/test_encripta.py
+++ tests/test_encripta.py
@@ -1,5 +1,18 @@
 from cesar.cesar import encripta
 
 
-def test_encripta_clayton_retorna():
-    assert encripta('Clayton') == 'huscfa'
+def test_encripta_clayton_retorna_pynlgba():
+    assert encripta('clayton') == 'pynlgba'
+
+
+def test_encripta_pynlgba_retorna_clayton():
+    assert encripta('pynlgba') == 'clayton'
+
+
+def test_encripta_deve_retornar_minusculas():
+    assert encripta('A').islower()
+
+
+def test_encripta_deve_preservar_os_espacos():
+    resultado = encripta('e a')
+    assert resultado[1] == ' '
